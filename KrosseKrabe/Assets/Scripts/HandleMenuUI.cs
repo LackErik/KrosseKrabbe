1c423e27b6c215e2039fabace70126ca370f8e8d
Jannik Hoefener <82703032+JannikHoefener@users.noreply.github.com>
2023-01-02 18:10:42 +0100

    Ingame Menu erstellt #4
    neues Script (handleMenuUI) für Menu und SceneWechsel
    beides jz zusammen also MenuHandler und Scenen wechsel funktionen
    - berücksichtigt wurde das die mouse aktiviert und deaktiviert wird

---
+++ KrosseKrabe/Assets/Scripts/HandleMenuUI.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HandleMenuUI : MonoBehaviour
+{
+    //public static bool MenuIsActive = false;
+    public GameObject menuUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Zum Strtzeitpunkt soll das Menü geschlossen sein
+        menuUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 1. mal esc => Menü offen
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            // Menü aktivieren
+            menuUI.SetActive(true);
+            // Mouse Interaktion solange das Menü offen ist erlauben sonst wie "inGame"
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+        
+
+    }
+    public void CloseMenuByBotton() {
+        menuUI.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // handle Scenes:
+    public void ChangeSceneToLobby() {
+        SceneManager.LoadScene("Lobby");
+        menuUI.SetActive(false);
+    }
+
+    public void ChangeSceneToKitchen() {
+        SceneManager.LoadScene("Kitchen");
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
