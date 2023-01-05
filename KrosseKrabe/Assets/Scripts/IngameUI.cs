09a475f97fbc94f0de30633588e7c800c0adbfc1
Jannik Hoefener <82703032+JannikHoefener@users.noreply.github.com>
2023-01-05 13:48:34 +0100

    IngameUI Script + Struktur #5
    + IngameUI Script (Total / Score fehlt noch)
    + gameObjects sortiert in der Scene Kitchen
    => Empy Objects als Überordner ... später können die GameObjects dort auch rein verlegt werden

---
+++ KrosseKrabe/Assets/Scripts/IngameUI.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class IngameUI : MonoBehaviour
+{
+// 1. Initialisierungen
+    // Instruction (Text)
+    // [SerializeField] private TextMeshProUGUI instructionLabel; // Immoment nicht vorhanden
+
+    // Error Count UI (Counter)
+    [SerializeField] private TextMeshProUGUI errorCountLabel = null;
+
+    // Help Count UI (Counter)
+    [SerializeField] private TextMeshProUGUI helpCountLabel = null;
+    
+
+    // Help and Error Text UI (Text)
+    [SerializeField] private TextMeshProUGUI errorLabel;
+    [SerializeField] private float errorDisplayTime = 5.0f;
+    [SerializeField] private TextMeshProUGUI helpLabel;
+    [SerializeField] private float helpDisplayTime = 5.0f;
+
+    // Result / Score (when trainig finished) 
+    // [SerializeField] private string totalErrorPrefix = "Total Errors: "; // Immoment nicht vorhanden
+    // [SerializeField] private string totalHelpCountPrefix = "Total Help: "; // Immoment nicht vorhanden
+    // [SerializeField] private TextMeshProUGUI totalErrorCountLabel = null; // Immoment nicht vorhanden
+    // [SerializeField] private TextMeshProUGUI totalHelpCountLabel = null; // Immoment nicht vorhanden
+
+// 2. Funktionen 
+    private void  Awake()
+    {
+        //instructionLabel.SetText("");
+        errorLabel.SetText("");
+        helpLabel.SetText("");
+        helpCountLabel.SetText("0");
+        errorCountLabel.SetText("0");
+    }
+    void Start(){}
+    void Update(){}
+
+    // 2.1 handle Help and Error Text + Counter
+    
+    private IEnumerator DisplayForDuration(TextMeshProUGUI label, string msg, float duration)
+        {
+            label.text = msg;
+            yield return new WaitForSeconds(duration);
+            label.text = "";
+        }
+    public void DisplayHelp(string helpMsg, int helpCount)
+        {
+            StopHelpAndErrorDisplay();
+            helpCountLabel.text = helpCount + "";
+            StartCoroutine(DisplayForDuration(helpLabel, helpMsg, helpDisplayTime));
+        }
+
+        public void DisplayError(string errorMsg, int errorCount)
+        {
+            StopHelpAndErrorDisplay();
+            errorCountLabel.text = errorCount + "";
+            StartCoroutine(DisplayForDuration(errorLabel, errorMsg, errorDisplayTime));
+        }
+        public void StopHelpAndErrorDisplay()
+        {
+            StopAllCoroutines();
+            helpLabel.SetText("");
+            errorLabel.SetText("");
+        }
+
+    // 2.2 handle Instruction // Immoment nicht vorhanden
+    /*
+    public void DisplayInstruction(string instruction)
+        {
+            StopHelpAndErrorDisplay();
+            instructionLabel.SetText(instruction);
+        } 
+    */
+
+    // 2.3 handle Result / Score 
+    /*
+    public void SetResultsPanel(int totalErrorCount, int totalHelpCount)
+        {
+            totalErrorCountLabel.SetText(totalErrorPrefix + totalErrorCount);
+            totalHelpCountLabel.SetText(totalHelpCountPrefix + totalHelpCount);
+        }
+    */
+}
