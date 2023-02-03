0a17931ebeeecd49d306634edac75389632bc3be
LackErik <93646851+LackErik@users.noreply.github.com>
2023-02-03 13:18:06 +0100

    Sounds

--- KrosseKrabe/Assets/Scripts/Raycaster.cs
+++ KrosseKrabe/Assets/Scripts/Raycaster.cs
@@ -26,7 +26,10 @@ public class Raycaster : MonoBehaviour
     //handle RawMeat
     public GameObject meatGrillPosition;
     public GameObject rawMeat;
-    
+
+    public AudioSource korrektSound;
+    public AudioSource wrongSound;
+
 
     private void Awake()
     {
@@ -88,9 +91,11 @@ public class Raycaster : MonoBehaviour
             tempSelected = interactions[objectIndex].model;
 
             if (objectIndex < 9) {
+               
                 objectIndex++;
             }
-            
+            korrektSound.Play();
+
             Debug.Log("Nr." + objectIndex + " " + selectedGameObject.name);
         
             // txt.SetText("Nr." + objectIndex + " " + selectedGameObject.name);
@@ -100,6 +105,7 @@ public class Raycaster : MonoBehaviour
         {
             //UI Funktion für Error jedoch nur dann wenn falsch geklickt
             didError(selectedGameObject);
+            wrongSound.Play();
             Debug.Log("Flase: " + interactions[objectIndex].model);
            // txt.SetText("Wrong Number");
         }
