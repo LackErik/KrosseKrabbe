432bbd43e01fa716ede52c2b98b10a2b0d5cadb6
LackErik <93646851+LackErik@users.noreply.github.com>
2023-02-03 12:08:19 +0100

    Grilltimer + bugfix(petty zurückspringen) #7

--- KrosseKrabe/Assets/Scripts/GrillBehavior.cs
+++ KrosseKrabe/Assets/Scripts/GrillBehavior.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GrillBehavior : MonoBehaviour
 {
@@ -10,47 +11,106 @@ public class GrillBehavior : MonoBehaviour
     public GameObject burnedMeat;
     public float rawToCookedTime = 180f;
     public float CookedToBurnedTime = 60f;
+    public TextMeshProUGUI UIgrillTime;
+    public int waitTimer;
+    public int grillTime;
+    private float nextActionTime;
+    private float period;
+    private bool startCooking;
+  
 
     private void Awake()
     {
         rawMeat.SetActive(true);
         cookedMeat.SetActive(false);
         burnedMeat.SetActive(false);
+        
+        UIgrillTime.text = "start";
     }
-    
+    private void Start()
+    {
+        period = 1;
+        grillTime = 0;
+        startCooking = false;
+    }
+
     private void Update()
     {
+        showTimer();
+
         // 1. Wenn rawPatty auf dem Grill liegt, starte cooking
-        if(rawMeat.transform.position == meatGrillPosition.transform.position){
-            StartCoroutine(ReplaceWithCookedMeat());
+        if (rawMeat.transform.position == meatGrillPosition.transform.position){
+            startCooking = true;
+            // Sekunden bis Patty fertig
+            if(grillTime == 180)
+            {
+                rawMeat.SetActive(false);
+                cookedMeat.SetActive(true);
+                
+            }
+            // Sekunden bis Patty Verbrand
+            if (grillTime == 185 && cookedMeat.transform.position == meatGrillPosition.transform.position)
+            {
+               
+                cookedMeat.SetActive(false);
+                burnedMeat.SetActive(true);
+            }
+            
+  
         }
         
-        // 2. wenn Patty gekocht fertig, dann beginne "Verbennungszeit"
-        if(cookedMeat.activeSelf == true){
-            StartCoroutine(ReplaceWithBurnedMeat());
-        }
-        
-        // 3. Wenn Patty verbrannt, beende das Trainung und gebe Rückmeldung
-        if(burnedMeat.activeSelf == true){
-            // ... doSomething ...
-        }
+  
         
     }
 
-    private IEnumerator ReplaceWithCookedMeat()
+    private void showTimer()
     {
-        yield return new WaitForSeconds(rawToCookedTime);
+        // geschwindigkeit anpassen
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            period = 0.05f;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            period = 1;
+        }
+        //Timer Stopen
+        if (cookedMeat.transform.position != meatGrillPosition.transform.position)
+        {
+            startCooking = false;
+            UIgrillTime.text = "";
 
-        rawMeat.SetActive(false);
-        cookedMeat.SetActive(true);
-        //StartCoroutine(ReplaceWithBurnedMeat());
+        }
+        //Timer Anzeige
+        if (startCooking)
+        {
+
+            if (Time.time > nextActionTime)
+            {
+
+                grillTime++;
+                nextActionTime = Time.time + period;
+
+                if (grillTime < 60)
+                {
+                    if (grillTime < 10)
+                    {
+                        UIgrillTime.text = "0:0" + grillTime.ToString();
+                    }
+                    else { UIgrillTime.text = "0:" + grillTime.ToString(); }
+
+                }
+                else
+                {
+                    if ((grillTime % 60) < 10)
+                    {
+                        UIgrillTime.text = ((grillTime / 60) % 60) + ":0" + (grillTime % 60);
+                    }
+                    else { UIgrillTime.text = ((grillTime / 60) % 60) + ":" + (grillTime % 60); }
+
+                }
+            }
+        }
     }
 
-    private IEnumerator ReplaceWithBurnedMeat()
-    {
-        yield return new WaitForSeconds(CookedToBurnedTime);
-
-        cookedMeat.SetActive(false);
-        burnedMeat.SetActive(true);
-    }
 }
